35bcf7ec9d6c04ad539c3b9d97a5659a911b4379
Dmytro Yarmak <dmytroy@backbase.com>
2015-09-19 17:29:59 +0300

    More test cases

--- test/parallel-matrix-multiplication-spec.js
+++ test/parallel-matrix-multiplication-spec.js
@@ -8,20 +8,50 @@ describe('ParallelMatrixMultiplication', function() {
       expect(window.ParallelMatrixMultiplication.product).toEqual(jasmine.any(Function));
     });
 
-    it('calculate product of matrices 2x3 and 3x2', function() {
-      var matrixA = [[1, 2, 3], [4, 5, 6]];
-      var matrixB = [[7, 8], [9, 10], [11, 12]];
-      var expectedResult = [[58, 64], [139, 154]];
-      var actualResult = window.ParallelMatrixMultiplication.product(matrixA, matrixB);
-      expect(actualResult).toEqual(expectedResult);
-    });
+    describe('for matrices nxm and mxp', function() {
+      it('calculate product when n = p and n < m', function() {
+        var matrixA = [
+          [1, 2, 3],
+          [4, 5, 6]
+        ];
+        var matrixB = [
+          [7, 8],
+          [9, 10],
+          [11, 12]
+        ];
+        var expectedResult = [
+          [58, 64],
+          [139, 154]
+        ];
+        var actualResult = window.ParallelMatrixMultiplication.product(matrixA, matrixB);
+        expect(actualResult).toEqual(expectedResult);
+      });
 
-    it('throws error when columns of matrix A does not match rows of matrix B', function() {
-      var matrixA = [[1, 2], [3, 4]];
-      var matrixB = [[7, 8], [9, 10], [11, 12]];
-      expect(function() {
-        window.ParallelMatrixMultiplication.product(matrixA, matrixB);
-      }).toThrowError('Columns of first matrix should match rows of second matrix');
+      it('calculate product when n != p, n < m and m < p', function() {
+        var matrixA = [
+          [1, 2, 3],
+          [4, 5, 6]
+        ];
+        var matrixB = [
+          [ 7,  8,  9, 10],
+          [11, 12, 13, 14],
+          [15, 16, 17, 18]
+        ];
+        var expectedResult = [
+          [ 74,  80,  86,  92],
+          [173, 188, 203, 218]
+        ];
+        var actualResult = window.ParallelMatrixMultiplication.product(matrixA, matrixB);
+        expect(actualResult).toEqual(expectedResult);
+      });
+
+      it('throws error when columns of matrix A does not match rows of matrix B', function() {
+        var matrixA = [[1, 2], [3, 4]];
+        var matrixB = [[7, 8], [9, 10], [11, 12]];
+        expect(function() {
+          window.ParallelMatrixMultiplication.product(matrixA, matrixB);
+        }).toThrowError('Columns of first matrix should match rows of second matrix');
+      });
     });
   });
 });
